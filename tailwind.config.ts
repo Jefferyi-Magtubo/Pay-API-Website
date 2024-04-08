91245e46125fed6845174c5a943a82a16fb2a5e8
jffryi <jefferyi.magtubo@gmail.com>
2024-04-08 11:57:14 -0500

    Started project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "darkPink" : "#ba4270",
+        "linkWaterWhite" : "#fbfcefe",
+        "sanJuanBlue" : "#36536b",
+        "mirageBlue" : "#1b262f",
+        "charmPink" : "#da6d97",
+        "lightSanJuanBlue" : "#6c8294"
+      },
+      fontFamily: {
+        'athl' : ["Atkinson Hyperlegible", "sans-serif"]
+      },
+      screens: {
+        'xs' : '320px',
+        'smh' : '400px'
+      },
+      fontSize: {
+        'xxs' : '.5rem'
+      },
+      width: {
+        '45%': '45%',
+        '32%': '32%'
+      },
     },
   },
   plugins: [],
