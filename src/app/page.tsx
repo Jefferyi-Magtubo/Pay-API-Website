29251d7e415d19ea4837c7bcf14e784bd53143a7
jffryi <jefferyi.magtubo@gmail.com>
2024-04-09 20:27:15 -0500

    Started project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,24 @@ import { Main } from "next/document";
 
 export default function Home() {
   return (
-    <main className="relative ">
-
-        <div className=" absolute  w-full mx-auto  -bottom-20 ">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 780 780"  className="" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><circle id="a" cx="390" cy="390" r="390"/><circle id="b" cx="390" cy="390" r="390"/></defs><g fill="#36536B" fill-rule="evenodd" opacity=".15"><use xlinkHref="#a"/><use xlinkHref="#b"/></g></svg>
-        </div>
-
-        
-  
-    </main>
+<main className="relative w-screen ">
+  <div className="absolute w-full -top-24 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 780 780" 
+      className="w-full h-auto"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <circle id="a" cx="390" cy="390" r="390"/>
+        <circle id="b" cx="390" cy="390" r="390"/>
+      </defs>
+      <g fill="#36536B" fillRule="evenodd" opacity=".15">
+        <use xlinkHref="#a"/>
+        <use xlinkHref="#b"/>
+      </g>
+    </svg>
+  </div>
+</main>
   );
 }
